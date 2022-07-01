3a54c630d73dad7f70f8bfb0268397cb9d13771a
smartboy0310 <qdustudent6@gmail.com>
2022-07-01 16:08:53 +0500

    Edit localizations

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import './Country.css';
-import { Link } from 'react-router-dom';
+
+
+import Images1 from "../../Assets/Images/tashkent.png";
+import Images2 from "../../Assets/Images/samarqand.png";
+import Images3 from "../../Assets/Images/fargona.png";
+import Images4 from "../../Assets/Images/andijon.png";
+import Images5 from "../../Assets/Images/jizzah.png";
+import Images6 from "../../Assets/Images/navoiy.png";
+import Images7 from "../../Assets/Images/qarshi.png";
+import Images8 from "../../Assets/Images/surxon.png";
+import Images9 from "../../Assets/Images/buxoro.png";
+import Images10 from "../../Assets/Images/sirdaryo.png";
+import Images11 from "../../Assets/Images/namangan.png";
+import Images12 from "../../Assets/Images/xorazim.png";
+
 
 function Country() {
-	const country = [];
-	const obj = {
-		name: 'Viloyat nomi',
-		title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate quia distinctio odio, officia saepe culpa assumenda facilis eligendi autem voluptate?',
-		image: ' https://picsum.photos/300/300',
-	};
-	for (let i = 1; i <= 13; i++) {
-		country.push({
-			name: obj.name,
-			title: obj.title,
-			image: obj.image,
-		});
-	}
+	
 
 	return (
 		<>
@@ -24,39 +26,133 @@ function Country() {
 					
 				</div>
 				<div className="container">
-					<ul className="country__list">
-						{country &&
-							country.map((e, i) => (
-								<li key={i} className="country__item">
-									<Link
-										to="/province"
-										className="country__link"
-									>
-										<ul className="sub__list">
-											<li className="sub__item">
-												<img
-													src={e.image}
-													alt={e.name}
-													className="country__img"
-													width={300}
-													height={300}
-												/>
-											</li>
-											<li className="sub__item">
-												<div className="country__info">
-													<h2 className="country__heading">
-														{e.name}
-													</h2>
-													<p className="country__discription">
-														{e.title}
-													</p>
-												</div>
-											</li>
-										</ul>
-									</Link>
-								</li>
-							))}
-					</ul>
+				<div className="card__box">
+                    <h1 className="car__head">O’zbekiston viloyatlari</h1>
+                    <p className="card__text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+                    <ul className="card__list">
+                        <li className="card__item">
+                            <img className="card__item--image" src={Images1} alt="" />
+                            <h2 className="card__item--head">Toshkent viloyati</h2>
+                            <p className="card__item--text">Lorem Ipsum is simply dummy text of the printing.</p>
+                            <div className="item__box--wrapper">
+                                <div className="card__item--location">Shimoliy-sharqiy</div>
+                                <div className="card__item--num">231</div>
+                            </div>
+                            <a className="card__item--link" href="#">Batafsil</a>
+                        </li>
+                        <li className="card__item">
+                            <img className="card__item--image" src={Images2} alt="" />
+                            <h2 className="card__item--head">Samarqand viloyati</h2>
+                            <p className="card__item--text">Lorem Ipsum is simply dummy text of the printing.</p>
+                            <div className="item__box--wrapper">
+                                <div className="card__item--location">Shimoliy-sharqiy</div>
+                                <div className="card__item--num">231</div>
+                            </div>
+                            <a className="card__item--link" href="#">Batafsil</a>
+                        </li>
+                        <li className="card__item">
+                            <img className="card__item--image" src={Images3} alt="" />
+                            <h2 className="card__item--head">Farg'ona viloyati</h2>
+                            <p className="card__item--text">Lorem Ipsum is simply dummy text of the printing.</p>
+                            <div className="item__box--wrapper">
+                                <div className="card__item--location">Shimoliy-sharqiy</div>
+                                <div className="card__item--num">231</div>
+                            </div>
+                            <a className="card__item--link" href="#">Batafsil</a>
+                        </li>
+                        <li className="card__item">
+                            <img className="card__item--image" src={Images4} alt="" />
+                            <h2 className="card__item--head">Andijon viloyati</h2>
+                            <p className="card__item--text">Lorem Ipsum is simply dummy text of the printing.</p>
+                            <div className="item__box--wrapper">
+                                <div className="card__item--location">Shimoliy-sharqiy</div>
+                                <div className="card__item--num">231</div>
+                            </div>
+                            <a className="card__item--link" href="#">Batafsil</a>
+                        </li>
+                        <li className="card__item">
+                            <img className="card__item--image" src={Images5} alt="" />
+                            <h2 className="card__item--head">Jizzah viloyati</h2>
+                            <p className="card__item--text">Lorem Ipsum is simply dummy text of the printing.</p>
+                            <div className="item__box--wrapper">
+                                <div className="card__item--location">Shimoliy-sharqiy</div>
+                                <div className="card__item--num">231</div>
+                            </div>
+                            <a className="card__item--link" href="#">Batafsil</a>
+                        </li>
+                        <li className="card__item">
+                            <img className="card__item--image" src={Images6} alt="" />
+                            <h2 className="card__item--head">Navoiy viloyati</h2>
+                            <p className="card__item--text">Lorem Ipsum is simply dummy text of the printing.</p>
+                            <div className="item__box--wrapper">
+                                <div className="card__item--location">Shimoliy-sharqiy</div>
+                                <div className="card__item--num">231</div>
+                            </div>
+                            <a className="card__item--link" href="#">Batafsil</a>
+                        </li>
+                        <li className="card__item">
+                            <img className="card__item--image" src={Images7} alt="" />
+                            <h2 className="card__item--head">Qashqadaryo viloyati</h2>
+                            <p className="card__item--text">Lorem Ipsum is simply dummy text of the printing.</p>
+                            <div className="item__box--wrapper">
+                                <div className="card__item--location">Shimoliy-sharqiy</div>
+                                <div className="card__item--num">231</div>
+                            </div>
+                            <a className="card__item--link" href="#">Batafsil</a>
+                        </li>
+                        <li className="card__item">
+                            <img className="card__item--image" src={Images8} alt="" />
+                            <h2 className="card__item--head">Surxondaryo viloyati</h2>
+                            <p className="card__item--text">Lorem Ipsum is simply dummy text of the printing.</p>
+                            <div className="item__box--wrapper">
+                                <div className="card__item--location">Shimoliy-sharqiy</div>
+                                <div className="card__item--num">231</div>
+                            </div>
+                            <a className="card__item--link" href="#">Batafsil</a>
+                        </li>
+                        <li className="card__item">
+                            <img className="card__item--image" src={Images9} alt="" />
+                            <h2 className="card__item--head">Buxoro viloyati</h2>
+                            <p className="card__item--text">Lorem Ipsum is simply dummy text of the printing.</p>
+                            <div className="item__box--wrapper">
+                                <div className="card__item--location">Shimoliy-sharqiy</div>
+                                <div className="card__item--num">231</div>
+                            </div>
+                            <a className="card__item--link" href="#">Batafsil</a>
+                        </li>
+                        <li className="card__item">
+                            <img className="card__item--image" src={Images10} alt="" />
+                            <h2 className="card__item--head">Sirdaryo viloyati</h2>
+                            <p className="card__item--text">Lorem Ipsum is simply dummy text of the printing.</p>
+                            <div className="item__box--wrapper">
+                                <div className="card__item--location">Shimoliy-sharqiy</div>
+                                <div className="card__item--num">231</div>
+                            </div>
+                            <a className="card__item--link" href="#">Batafsil</a>
+                        </li>
+                        <li className="card__item">
+                            <img className="card__item--image" src={Images11} alt="" />
+                            <h2 className="card__item--head">Namangan viloyati</h2>
+                            <p className="card__item--text">Lorem Ipsum is simply dummy text of the printing.</p>
+                            <div className="item__box--wrapper">
+                                <div className="card__item--location">Shimoliy-sharqiy</div>
+                                <div className="card__item--num">231</div>
+                            </div>
+                            <a className="card__item--link" href="#">Batafsil</a>
+                        </li>
+                        <li className="card__item">
+                            <img className="card__item--image" src={Images12} alt="" />
+                            <h2 className="card__item--head">Xorazim viloyati</h2>
+                            <p className="card__item--text">Lorem Ipsum is simply dummy text of the printing.</p>
+                            <div className="item__box--wrapper">
+                                <div className="card__item--location">Shimoliy-sharqiy</div>
+                                <div className="card__item--num">231</div>
+                            </div>
+                            <a className="card__item--link" href="#">Batafsil</a>
+                        </li>
+                        
+                    </ul>
+                </div>
 				</div>
 			</section>
 		</>
--- src/Components/Province/ProvinceParks/ProvinceParks.jsx
+++ src/Components/Province/ProvinceParks/ProvinceParks.jsx
@@ -3,6 +3,7 @@ import './ProvinceParks.css';
 
 import parkOne from '../../../Assets/Images/Qoraqalpoq/Parks/amir-temur.jpg';
 import parkTwo from '../../../Assets/Images/Qoraqalpoq/Parks/istiqlol.jpg';
+import parkAdd from '../../../Assets/Images/location.svg';
 
 function ProvinceParks() {
 	return (
@@ -12,42 +13,66 @@ function ProvinceParks() {
 				<div className="parks__box">
 					<div className="parks__info-box">
 						<div className="parks__img-box">
-						<img
-							className="park-img"
-							src={parkOne}
-							alt="Recreation parks"
-							width={300}
-							height={350}
-						/>
+							<img
+								className="park-img"
+								src={parkOne}
+								alt="Recreation parks"
+								width={300}
+								height={350}
+							/>
 						</div>
 						<div className="park-info">
 							<h2 className="park-title">
-								Lorem ipsum dolor sit amet.
+								Amir Temur istirohat bog'i
 							</h2>
 							<p className="park-discription">
-								Lorem ipsum dolor sit, amet consectetur
-								adipisicing elit. Quos, alias.
+								Nukus shahrida bolalar va kattalar uchun yozgi
+								dam olish órni
 							</p>
+							<div className="park-address">
+								<img
+									src={parkAdd}
+									alt="Park address icon"
+									width={40}
+									height={40}
+									className="park-add__img"
+								/>
+								<span>
+									Nukus shahri A.Dosnazarov kóchasida joylashgan
+								</span>
+							</div>
 						</div>
 					</div>
 					<div className="parks__info-box">
-					<div className="parks__img-box">
-						<img
-							className="park-img"
-							src={parkTwo}
-							alt="Recreation parks"
-							width={300}
-							height={350}
-						/>
+						<div className="parks__img-box">
+							<img
+								className="park-img"
+								src={parkTwo}
+								alt="Recreation parks"
+								width={300}
+								height={350}
+							/>
 						</div>
 						<div className="park-info">
 							<h2 className="park-title">
-								Lorem ipsum dolor sit amet.
+								Istiqlol istirohat bogi
 							</h2>
 							<p className="park-discription">
-								Lorem ipsum dolor sit, amet consectetur
-								adipisicing elit. Quos, alias.
+								Nukus shahrida bolalar va kattalar uchun yozgi
+								dam olish órni
 							</p>
+							<div className="park-address">
+								<img
+									src={parkAdd}
+									alt="Park address icon"
+									width={40}
+									height={40}
+									className="park-add__img"
+								/>
+								<span>
+									Nukus shahri A.Utepov r/u
+								</span>
+								</div>
 						</div>
 					</div>
 				</div>
